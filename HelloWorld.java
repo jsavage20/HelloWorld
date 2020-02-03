42cf0e0bf2e3342240f0d350c607c4654f64cb5d
Your Name <you@example.com>
2020-02-03 18:18:14 +0000

    added main method

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,12 @@
 // Hello World Java
+import java.util.Scanner;
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Whats your name?");
+        String name = kb.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+}
